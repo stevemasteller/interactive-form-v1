6359fd1c2a1eff253fd0980b18e445adda3f19b9
stevemasteller <stevermasteller@gmail.com>
2016-09-11 00:07:15 -0400

    T-shirt design select element event handler
    Added event handler for T-Shirt design select element

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+
 var addOtherTitleMarkup = function() {
 	$newInput = $('<input id="other-title" placeholder="Your Title">');
 	
@@ -7,9 +8,51 @@ var addOtherTitleMarkup = function() {
 
 var removeOtherTitleMarkup = function() {
 	$('#other-title').remove();
-}
+};
 
-// Event handler for Job Role select element
+var displayJsPunsColors = function() {
+	alert();
+	$('#color').children('option').eq(0).removeAttr('style');
+	$('#color').children('option').eq(1).removeAttr('style');
+	$('#color').children('option').eq(2).removeAttr('style');
+	$('#color').children('option').eq(3).attr('style', 'display: none');
+	$('#color').children('option').eq(4).attr('style', 'display: none');
+	$('#color').children('option').eq(5).attr('style', 'display: none');
+};
+
+var displayHeartJsColors = function() {
+	$('#color').children('option').eq(0).attr('style', 'display: none');
+	$('#color').children('option').eq(1).attr('style', 'display: none');
+	$('#color').children('option').eq(2).attr('style', 'display: none');
+	$('#color').children('option').eq(3).removeAttr('style');
+	$('#color').children('option').eq(4).removeAttr('style');
+	$('#color').children('option').eq(5).removeAttr('style');
+};
+
+var displayThemeColors = function() {
+	$('#color').children('option').eq(0).removeAttr('style');
+	$('#color').children('option').eq(1).removeAttr('style');
+	$('#color').children('option').eq(2).removeAttr('style');
+	$('#color').children('option').eq(3).removeAttr('style');
+	$('#color').children('option').eq(4).removeAttr('style');
+	$('#color').children('option').eq(5).removeAttr('style');
+};
+
+// Event handler for T-Shirt design select element
+$('#design').change( function () {
+		var selectVal = $('#design').find(':selected').val();
+		
+		alert(selectVal);
+		if (selectVal === 'js puns') {
+			displayJsPunsColors();
+		} else if (selectVal === 'heart js') {
+			displayHeartJsColors();
+		} else {
+			displayThemeColors();
+		}
+	});
+
+// Event handler for Job Role title select element
 $('#title').change( function() {
 		var selectedVal = $('#title').find(':selected').val();
 		
