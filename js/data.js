49f33c4477ea291a2bb376edc7737557c7c83cb6
stevemasteller <stevermasteller@gmail.com>
2016-09-11 10:49:41 -0400

    Refactored everything

--- js/data.js
+++ js/data.js
@@ -1,14 +1,46 @@
-const MAIN_CONFERANCE = '0';
-const
-const
-const
-const
-const
-const
+const MAIN_CONFERENCE 		= '0';
+const FRAMEWORKS_WORKSHOP	= '1';
+const LIBRARIES_WORKSHOP	= '2';
+const EXPRESS_WORKSHOP		= '3';
+const NODE_JS_WORKSHOP		= '4';
+const BUILD_TOOLS_WORKSHOP	= '5';
+const NPM_WORKSHOP 			= '6';
 
 
-var activities = [
+var activitiesArray = [
 {
-	activity: MAIN
-}
+	activity: MAIN_CONFERENCE,
+	conflicts: [],
+	cost: 200,
+},
+{
+	activity: FRAMEWORKS_WORKSHOP,
+	conflicts: [EXPRESS_WORKSHOP, BUILD_TOOLS_WORKSHOP],
+	cost: 100,
+},
+{
+	activity: LIBRARIES_WORKSHOP,
+	conflicts: [NODE_JS_WORKSHOP, NPM_WORKSHOP],
+	cost: 100,
+},
+{
+	activity: EXPRESS_WORKSHOP,
+	conflicts: [FRAMEWORKS_WORKSHOP, BUILD_TOOLS_WORKSHOP],
+	cost: 100,
+},
+{
+	activity: NODE_JS_WORKSHOP,
+	conflicts: [LIBRARIES_WORKSHOP, NPM_WORKSHOP],
+	cost: 100,
+},
+{
+	activity: BUILD_TOOLS_WORKSHOP,
+	conflicts: [FRAMEWORKS_WORKSHOP, EXPRESS_WORKSHOP],
+	cost: 100,
+},
+{
+	activity: NPM_WORKSHOP,
+	conflicts: [LIBRARIES_WORKSHOP, NODE_JS_WORKSHOP],
+	cost: 100,
+},
 ]
--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
 
 
-var addOtherTitleMarkup = function() {
-	$newInput = $('<input id="other-title" placeholder="Your Title">');
-	
-	$('#title').after($newInput);
-};
-
-var removeOtherTitleMarkup = function() {
-	$('#other-title').remove();
-};
-
 var displayJsPunsColors = function() {
-	alert();
 	$('#color').children('option').eq(0).removeAttr('style');
 	$('#color').children('option').eq(1).removeAttr('style');
 	$('#color').children('option').eq(2).removeAttr('style');
@@ -38,27 +27,79 @@ var displayThemeColors = function() {
 	$('#color').children('option').eq(5).removeAttr('style');
 };
 
-var enableDisableActivities = function(index) {
-	var selectChecked = $('.activities').find('input').eq(index).prop('checked');
+// Event handler for T-Shirt design select element
+$('#design').change( function () {
+	var selectVal = $('#design').find(':selected').val();
 	
-	for (var i = 1; i < arguments.length; i++) {
-		$('.activities').find('input').eq(arguments[i]).prop('disabled', selectChecked);
-		
-		if (selectChecked) {
-			$('.activities').find('label').eq(arguments[i]).attr('style', 'color: rgb(84, 84, 84)');
-		} else {
-			$('.activities').find('label').eq(arguments[i]).attr('style', 'color: black');
-		}
+	if (selectVal === 'js puns') {
+		displayJsPunsColors();
+	} else if (selectVal === 'heart js') {
+		displayHeartJsColors();
+	} else {
+		displayThemeColors();
 	}
+});
+/*****************************************
+*
+******************************************/
+var addOtherTitleMarkup = function() {
+	$newInput = $('<input id="other-title" placeholder="Your Title">');
+	
+	$('#title').after($newInput);
+};
+
+var removeOtherTitleMarkup = function() {
+	$('#other-title').remove();
+};
+
+// Event handler for Job Role select element
+$('#title').change( function() {
+	var selectedVal = $('#title').find(':selected').val();
+	
+	if (selectedVal === 'other') {
+		addOtherTitleMarkup();
+	} else {
+		removeOtherTitleMarkup();
+	}
+});
+	
+/*****************************************
+*
+******************************************/
+var enableActivities = function() {
+	
+	// Set disabled and color of all inputs to enabled 
+	for (var j = 0; j < activitiesArray.length; j++) {
+	    $('.activities').find('input').eq(j).prop('disabled', false);
+		$('.activities').find('label').eq(j).attr('style', 'color: black');
+	}
+	
 }
 
-//Event handlers for Workshops
-$('.activities').find('input').eq(1).change(function() {enableDisableActivities(1,3,5)});
-$('.activities').find('input').eq(2).change(function() {enableDisableActivities(2,4,6)});
-$('.activities').find('input').eq(3).change(function() {enableDisableActivities(3,1,5)});
-$('.activities').find('input').eq(4).change(function() {enableDisableActivities(4,2,6)});
-$('.activities').find('input').eq(5).change(function() {enableDisableActivities(5,1,3)});
-$('.activities').find('input').eq(6).change(function() {enableDisableActivities(6,2,4)});
-	
+var disableActivities = function() {
+	// disable select checboxes depending on which boxes are checked
+	for (var j = 0; j < activitiesArray.length; j++) {
+		var isChecked = $('.activities').find('input').eq(j).prop('checked');
+		
+		if (isChecked) {
+			for (var i = 0; i < activitiesArray[j].conflicts.length; i++) {
+				var conflictingActivity = activitiesArray[j].conflicts[i];
+				
+				$('.activities').find('input').eq(conflictingActivity).prop('disabled', true);
+				$('.activities').find('label').eq(conflictingActivity).attr('style', 'color: rgb(84, 84, 84)');
+			}
+		}
+	}	
+}
+
+// Event handler for Activities check boxes
+$('.activities').on('click', 'input[type="checkbox"]', function() {
+	enableActivities();
+	disableActivities();
+});
+
+/*****************************************
+*
+******************************************/
 // On load
 $('#name').focus();
