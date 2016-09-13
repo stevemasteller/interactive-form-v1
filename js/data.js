e6126a271f7b2986bb6db7eea9d21b85a97ba782
stevemasteller <stevermasteller@gmail.com>
2016-09-13 04:12:23 -0400

    added comments to data.js file

--- js/data.js
+++ js/data.js
@@ -3,14 +3,14 @@
 //		shows all conflicts
 //		and cost of each activity
 const MAIN_CONFERENCE 		= '0';	// numeric constants identifying each activity
-const FRAMEWORKS_WORKSHOP	= '1';
-const LIBRARIES_WORKSHOP	= '2';
+const FRAMEWORKS_WORKSHOP	= '1';  // must correspond to index in array and order
+const LIBRARIES_WORKSHOP	= '2';	// displayed on form
 const EXPRESS_WORKSHOP		= '3';
 const NODE_JS_WORKSHOP		= '4';
 const BUILD_TOOLS_WORKSHOP	= '5';
 const NPM_WORKSHOP 			= '6';
 
-
+// global array of objects identifying activity conflicts and prices
 var activitiesArray = [
 {
 	activity: MAIN_CONFERENCE,
